679d304c097115df5455a0ae2ea5ddb93b9ee63b
AustinPotts <pottsevolvements@gmail.com>
2019-11-06 15:14:07 -0500

    Configured Serial Queue

--- ConcurrencyIII.playground/Pages/Untitled Page.xcplaygroundpage/Contents.swift
+++ ConcurrencyIII.playground/Pages/Untitled Page.xcplaygroundpage/Contents.swift
@@ -1,3 +1,35 @@
 import Foundation
 
+//Atomic
 
+class Atomic<Value> {
+    
+    private var value: Value
+    private let queue = DispatchQueue(label: "Atomic \(Value.self)") //Because we are adding this serial queue, everything has to happen one at a time
+    
+    init(value: Value){
+        self.value = value
+    }
+    
+    func modify(modifier: (Value)-> Value){ //Using closure to set new value
+        queue.sync {
+            value = modifier(value)
+        }
+        
+    }
+    
+    func get()->Value{
+       // return value
+        return queue.sync { value }
+    }
+    
+    
+}
+
+let sharedValue = Atomic(value: 40)
+
+DispatchQueue.concurrentPerform(iterations: 100) { (_) in
+    sharedValue.modify{ $0 + 1 } //This is the modification we are making
+}
+
+print("Final value: \(sharedValue.get())") //Multiple threads will be trying to access this at the same time causeing a wrong value count
